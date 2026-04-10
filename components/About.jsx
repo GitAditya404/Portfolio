ae27bd749f5e1d4e71f6c1d09d1506e26755ffa4
Aditya Kumar <aditya.work404@gmail.com>
2026-04-10 09:24:29 +0530

    skill: made it responsive for mobile device

--- components/About.jsx
+++ components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     const {myStyle} = useContext(darkContext)
 
   return (
-    <div id='About' className='sm:h-screen ml-6 mt-24 sm:mt-0 sm:ml-[25vw] scroll-mt-20 sm:mr-76 ' style={myStyle}>
+    <div id='About' className=' ml-6 mt-24 sm:mt-0 sm:ml-[25vw] scroll-mt-20 sm:mr-76 ' style={myStyle}>
         <h1 className='underline decoration-wavy text-3xl'>About Me</h1>
         <div className=' mt-7'>
             <p className='text-xl'>I am a Computer Science undergraduate currently in my pre-final year, based in Dehradun, India.</p>
@@ -18,45 +18,45 @@ const About = () => {
              </p>
         </div>
 
-        <div className=' text-center mt-9 '>
-        <p className='text-2xl  underline'>Skills</p>
+        <div className=' text-center mt-24 '>
+        <p className='text-2xl font-bold  underline'>Skills</p>
         </div>
-        <div className='grid grid-cols-1  mt-8 sm:grid-cols-2 md:grid-cols-3 gap-4 auto-rows-[150px] '>
+        <div className='grid grid-cols-1 mt-8 mr-9 ml-5 sm:ml-0 sm:mr-0 sm:grid-cols-2 md:grid-cols-3 gap-4 auto-rows-[150px] '>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">    
                 <i class="devicon-react-original text-6xl colored"></i>
-                <p>ReactJs</p>
+                <p className='text-lg sm:text-xl'>ReactJs</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">       
                 <i class="devicon-mongodb-plain text-6xl colored"></i>
-                <p>MongoDb</p>
+                <p className='text-lg sm:text-xl'>MongoDb</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">      
               <i class="devicon-express-original text-6xl colored"></i>
-                <p>Express</p>
+                <p className='text-lg sm:text-xl'>Express</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">    
                 <i class="devicon-css3-plain colored text-6xl"></i>
-                <p>CSS</p>
+                <p className='text-lg sm:text-xl'>CSS</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">    
-                <i class="devicon-html5-plain colored text-6xl"></i>
-                <p>HTML</p>
+                <i class="devicon-cplusplus-plain colored invert brightness-200  text-6xl"></i>
+                <p className='text-lg sm:text-xl'>C++</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">    
                 <i class="devicon-tailwindcss-original colored text-6xl"></i>
-                <p>Tailwind Css</p>
+                <p className='text-lg sm:text-xl'>Tailwind Css</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">    
                 <i class="devicon-github-original invert brightness-200 colored text-6xl"></i>
-                <p>Github</p>
+                <p className='text-lg sm:text-xl'>Github</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">    
                 <i class="devicon-javascript-plain colored text-6xl"></i>
-                <p>JavaScript</p>
+                <p className='text-lg sm:text-xl'>JavaScript</p>
               </div>
               <div className="border border-[#320273] rounded-2xl flex flex-col items-center justify-center">    
                 <i class="devicon-nodejs-plain-wordmark colored text-7xl"></i>
-                <p>NodeJs</p>
+                <p className='text-lg sm:text-xl'>NodeJs</p>
               </div>
 
         </div>
