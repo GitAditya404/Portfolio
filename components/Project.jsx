eaef80d4b745dd395f2aa8c4c29f816bbb3be7aa
Aditya Kumar <aditya.work404@gmail.com>
2026-04-10 16:28:34 +0530

    Project: made layout responsive for smartphone

--- components/Project.jsx
+++ components/Project.jsx
@@ -6,23 +6,23 @@ const Project = () => {
   const {myStyle} = useContext(darkContext)
   
   return (
-    <div id='Project' style={myStyle} className='scroll-mt-16'>
-        <div className=' min-h-screen ml-[25vw]  mt-4 mr-[15vw] bg-blue-500' style={myStyle}>
-            <h1 className='underline text-xl'>Projects</h1>
-            <div className='border flex border-black h-44 w-[50vw] mt-6'>
-              <div className='IMAGE w-[25vw] h-44 border border-amber-500'>
-                <img src="#" alt="" />
-              </div>
-              <div className='PROJECTDESCRIPTION w-[25vw] h-44 border border-b-fuchsia-400'></div>
+    <div id='Project' style={myStyle} className='scroll-mt-20 border-2 mt-24'>
+        <div className='ml-9 mr-7 sm:ml-[25vw] border-4 border-red-500 mt-4 sm:mr-72 bg-blue-500' style={myStyle}>
+            <div className='text-center'>
+              <h1 className='underline decoration-wavy text-3xl'>Projects</h1>
+            </div>
+            <div className='sm:flex sm:w-[50vw] mt-6'>
+                <div className='IMAGE sm:w-[25vw] h-72 border border-amber-500'>
+                  <img src="#" alt="" />
+                </div>
+                <div className='PROJECTDESCRIPTION sm:w-[25vw] h-72 border-2'>descript1</div>
             </div>
 
-            <div className='border flex border-black h-45 w-[50vw] mt-6'>
-              <div className='IMAGE w-[25vw] h-44 border border-amber-500'>
-
-              </div>
-              <div className='PROJECTDESCRIPTION w-[25vw] h-44 border border-b-fuchsia-400'>
-                <img src="#" alt="" />
-              </div>
+            <div className='sm:flex sm:w-[50vw] mt-6'>
+                <div className='IMAGE sm:w-[25vw] h-72 border border-amber-500'>
+                  <img src="#" alt="" />
+                </div>
+                <div className='PROJECTDESCRIPTION sm:w-[25vw] h-72 border '>descript2</div>
             </div>
         </div>
     </div>
