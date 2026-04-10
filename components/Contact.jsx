e4ed1c4a8501716e2e5c46c4befb9dcd0a4f2c53
Aditya Kumar <aditya.work404@gmail.com>
2026-04-10 23:37:46 +0530

    fixed layout of contact page

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,34 +12,35 @@ const Contact = () => {
   }
 
   return (
-    <div id='Contact' className='scroll-mt-16 w-full h-96  mt-7 bg-emerald-300' style={myStyle}> 
-        <div className='ml-[25vw] h-96 mr-[15vw] flex flex-col  border border-blue-500 '>
-          <h1 className='text-xl'>Based in India</h1>
+    <div id='Contact' className='scroll-mt-20 mt-20' style={myStyle}> 
+        <div className='ml-6 sm:ml-[25vw]  mr-5  border-red-600 sm:mr-80 flex flex-col  '>
+          <h1 className='text-3xl sm:text-4xl underline'>Based in India</h1>
 
-          <div className=' rounded-2xl overflow-hidden p-4  mt-3' style={contactSyle}>
+          <div className=' rounded-2xl border overflow-hidden p-4  mt-7' style={contactSyle}>
               
-              <div className='TEXTDIV text-xs rounded-2xl gap-2.5 flex flex-col  '>
-                  <p className ='underline text-sm' style={contactSyle}>Let's Connect</p>
+              <div className='TEXTDIV text-xs rounded-2xl gap-6 flex flex-col '>
+                  <div className='text-center'>
+                   <p className ='font-bold text-2xl' style={contactSyle}>Let's Connect</p>
+                  </div>
                   
-                  <div className='flex justify-between border-b border-gray-400 py-2' style={contactSyle}>
-                    <p className=''>Email</p>
-                    <p>aditya.work404@gmail.com</p>
+                  <div className='sm:flex justify-between border-b border-gray-400 py-4' style={contactSyle}>
+                    <p className='text-xl font-semibold'>Email</p>
+                    <p className='text-xl'>aditya.work404@gmail.com</p>
                   </div>
 
-                  <div className='flex justify-between border-b border-gray-400 py-2' style={contactSyle}>
-                    <p className='' >LinkedIn</p>
-                    <a href="https://www.linkedin.com/in/aditya-kumar-412877292" target='_blank'>www.linkedin.com/in/aditya-kumar-412877292</a>
+                  <div className='sm:flex justify-between border-b border-gray-400 py-4' style={contactSyle}>
+                    <p className='text-xl font-semibold' >LinkedIn</p>
+                    <a className='text-xl' href="https://www.linkedin.com/in/aditya-kumar-412877292" target='_blank'>www.linkedin.com/in/aditya-kumar-412877292</a>
                   </div>
 
-                  <div className='flex justify-between border-b border-gray-400 py-2' style={contactSyle}>
-                    <p className=''>Github</p>
-                    <a href="https://github.com/GitAditya404">https://github.com/GitAditya404</a>
+                  <div className='sm:flex justify-between border-b border-gray-400 py-5' style={contactSyle}>
+                    <p className='text-xl font-semibold'>Github</p>
+                    <a className='text-xl' href="https://github.com/GitAditya404">https://github.com/GitAditya404</a>
                   </div>
               </div>
           </div>
-          <div className='border overflow-hidden   border-amber-500 mt-auto  '>
-
-            <p className='text-9xl leading-none ml-8 translate-y-8'>ADITYA</p>
+          <div className=' overflow-hidden  border-amber-500 mt-7'>
+            <p className='text-8xl translate-y-6 sm:text-[230px] sm:translate-y-16 leading-none ml-3 '>ADITYA</p>
           </div>
         </div>
     </div>
--- components/Project.jsx
+++ components/Project.jsx
@@ -6,7 +6,7 @@ const Project = () => {
   const {myStyle} = useContext(darkContext)
   
   return (
-    <div id='Project' style={myStyle} className='scroll-mt-20 border-2 mt-24'>
+    <div id='Project' style={myStyle} className='scroll-mt-20 mt-24'>
         <div className='ml-9 mr-7 sm:ml-[25vw] border-4 border-red-500 mt-4 sm:mr-72 bg-blue-500' style={myStyle}>
             <div className='text-center'>
               <h1 className='underline decoration-wavy text-3xl'>Projects</h1>
